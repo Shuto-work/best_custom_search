96978be1fad23889e771584055619335330572ca
Shuto-work <shuto.work2828@gmail.com>
2024-09-03 16:12:40 +0900

    fix_pillow
    pillow dawngrade to 7.0.0

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request, session, url_for
+from flask import Flask, redirect, session, url_for
 from authlib.integrations.flask_client import OAuth
 import streamlit as st
 
